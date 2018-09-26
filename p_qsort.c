fd5b7bfab2d17f2bf28261a42c1fd28db72f80c9
Maria Pia Sanchez <mapisanchezg@gmail.com>
2018-09-25 23:33:12 -0300

    p_qsort.c in progress

--- p_qsort.c
+++ p_qsort.c
@@ -7,22 +7,29 @@
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
-#include "types.h"
-#include "const.h"
-#include "util.h"
 #include <ctype.h>
 #include <pthread.h>
 #include <limits.h>
 #include <signal.h>
 
-void swap(UINT *A, int a, int b) {
-    UINT tmp;
+int MAXTHREADS;
+int n;
+int current_thread = 0;
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+
+unsigned int *A;
+
+void swap(unsigned int *A, int a, int b) {
+    unsigned int tmp;
     tmp = A[a]; 
     A[a] = A[b]; 
     A[b] = tmp;
 }
 
-int partition (UINT* A, int lo, int hi) {
+int partition (unsigned int* A, int lo, int hi) {
     int pivot = A[hi]; // pivot
     int i = (lo - 1);  // Index of smaller element
     
@@ -40,7 +47,7 @@ int partition (UINT* A, int lo, int hi) {
 }
 
 // TODO: implement
-int quicksort (UINT* A, int lo, int hi) {
+int quicksort (unsigned int* A, int lo, int hi) {
     if (lo < hi){
         /* pi es partitioning index, arr[p] is now
            at right place */
@@ -57,47 +64,52 @@ int quicksort (UINT* A, int lo, int hi) {
 
 typedef struct {
     UINT *array;
-    int low;
-    int high;
+    int n;
     int pivot;
     int n_less_than_pivot;
-    int n_thread;
-} arguments;
+} args;
 
 
-void* parallel_partition(void *args){
-    arguments *information = args;
-    int lo, hi, piv, nums_less_than_pivot, thr;
-    UINT *A;
+pthread_t *separate_pthreads(pthread_t* threads, int first, int last){
+    pthread_t *new_array_pthrads;
 
-    lo  =      information->low;
-    hi  =     information->high;
-    piv =    information->pivot;
-    A   =    information->array;
-    thr = information->n_thread;
+    new_array_pthrads = malloc(sizeof(UINT)*(last-first));
 
-    nums_less_than_pivot = information->n_less_than_pivot;
+    for(size_t i = first; i <= last; i++)
+    {
+        new_array_pthrads[i] = threads[i];
+    }
+    return new_array_pthrads;
+}
 
-	int thread_part = n/MAXTHREADS; // cambiar n por el largo de A
-	unsigned int *local_arr = malloc(sizeof(int) * thread_part);
-	int cnt = 0;
-	for (int i = current_thread * thread_part; i < (current_thread + 1) * thread_part  && cnt < thread_part; ++i) {
-		local_arr[cnt] = A[i];
-		cnt++;		
-	}
-	
-	int k = -1;
-	for (int j = 0; j <= thread_part; j++){
+void* parallel_partition(void *arg){
+    args *info = arg;
+    int n = info->n;
+    int pivot = info->pivot;
+
+    int thread_part = n/MAXTHREADS;
+    int cnt = 0;
+
+    unsigned int *local_arr = malloc(sizeof(int) * thread_part);
+    
+    for (int i = current_thread * thread_part; i < (current_thread + 1) * thread_part  && cnt < thread_part; ++i) {
+        local_arr[cnt] = A[i];
+        cnt++;
+    }
+    
+    int k = -1;
+    for (int j = 0; j <= thread_part; j++) {
         if (local_arr[j] <= pivot){
-			k++;
+            k++;
             swap(local_arr, k, j);
         }
     }
-    k++;
-    
+   
+    k++; //numero de valores menores o iguales al pivote
+
     /* 2. Guardar la cantidad en un arreglo |S_i| */
     // (hacer una variable global que se pueda hacer malloc y free)
-
+    
     /* 3. Guardar posiciones en prefix sum */
     // Empieza de cero y se suma con el primer numero del arreglo anterior
     
@@ -108,33 +120,32 @@ void* parallel_partition(void *args){
     /* 2, 3 y 4 se debe hacer junto con los demás threads. La idea es que según el 
        numero de thread, que se posicione la cantidad en |S_i| */
     /* --- de alguna forma hay que usar variables de condicion, un thread jefe y un mutex --- */        
+
+    free(local_arr);
+    current_thread++;
+    pthread_exit(0);
 }
 
-int q_threads_per_array(int len_old_array, int len_new_array){
-
-
-}
-
-
-int parallel_quicksort(UINT* A, int lo, int hi, pthreads_t threads[], int q_threads, int size_arr){
-
+int parallel_quicksort(unsigned int* A, int lo, int hi, pthread_t threads[], int q_threads, int size_arr){
+    int pivot = (hi + lo) / 2;
     /* cuando el mutex este en 1 */
-
-    int last_positioned;
-    int q_threads_to_right;
-
-    /* hacer que los threads lean su parte del arreglo */
-    for(int i = 0; i < q_threads; i++){
-        /* threads leyendo numeros menores al pivote */
-        /* cambiarlo a pthread_create y poner los attr en un struct */
-        int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
-                          (void *) parallel_partition, void *arg);
-
-        // parallel_partition(A, size_arr * i , size_arr * (i + 1) - 1, threads[i], pivot);
-        
+    args *info = malloc(sizeof(args));
+    info->n = n;
+    info->pivot = pivot;
+    for (int i = 0; i < MAXTHREADS; ++i) {
+        pthread_mutex_lock(&lock);
+        if (pthread_create(&threads[i], NULL, (void*)parallel_partition, info)) {
+            free(info);         
+        }
+        pthread_join(threads[i], NULL);
+        pthread_mutex_unlock(&lock);
     }
 
     pthreads_t threads_to_left[], threads_to_right[];
+    int q_to_left; // ultimo del S_i
+    
+    threads_to_left = separate_pthreads(threads, first, q_to_left);
+    threads_to_left = separate_pthreads(threads, q_to_left, hi - q_to_left);
 
     parallel_quicksort(A, lo, last_positioned, threads_to_right, q_threads_to_right);
     parallel_quicksort(A, last_positioned + 1, hi, threads_to_left, q_threads - q_threads_to_right);
